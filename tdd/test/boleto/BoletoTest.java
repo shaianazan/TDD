168003c4bc92d5fabfa091469abcbcb3ba2ae950
shaiana <shaianazan@id.uff.br>
2021-03-07 19:55:21 -0300

    testar se fatura está paga com boletos de valor maior que o da fatura

--- tdd/test/boleto/BoletoTest.java
+++ tdd/test/boleto/BoletoTest.java
@@ -46,4 +46,26 @@ public class BoletoTest {
 		Assertions.assertEquals(expected, processadorBoleto.getSumPaymentsValue(listaBoletos));
 	}
 	
+	
+	@DisplayName("Testar boletos pagos com valor maior que o da fatura")
+	@Test
+	public void testaPagamentoMaior() {
+		listaBoletos.add(new Boleto("111101", new Date(), 1000.00));
+		listaBoletos.add(new Boleto("111101", new Date(), 500.00));
+		listaBoletos.add(new Boleto("111101", new Date(), 500.00));
+
+
+		Assertions.assertEquals(StatusFatura.CLOSED, processadorBoleto.processPayment(fatura, listaBoletos).getStatus());
+	}
+	
+	
+	
+	
+	
+
+
+
+
 }
+	
+
