bd7431c157a976de201b96211de158e7ba01d461
shaiana <shaianazan@id.uff.br>
2021-03-08 00:58:19 -0300

    implementação da classe boleto

--- tdd/src/boleto/Boleto.java
+++ tdd/src/boleto/Boleto.java
@@ -1,5 +1,48 @@
 package boleto;
 
-public class Boleto {
+import java.util.Date;
 
+
+public class Boleto {
+	
+	private String codigo;
+	private Date dataPagamento;
+	private double valor;
+
+	public Boleto(String codigo, Date dataPagamento, double valor) {
+	
+		super();
+		
+		this.codigo = codigo;
+		this.dataPagamento = dataPagamento;
+		this.valor = valor;
+		
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+	
 }
